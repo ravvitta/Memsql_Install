ccbad6f82a0b2826a87b51b96582d809ca459239
ravvitta <ravivittalstar@gmail.com>
2020-07-29 10:36:29 -0700

    MemSQL Install

--- postinstall_nonmaster.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-sudo yum-config-manager --add-repo https://release.memsql.com/production/rpm/x86_64/repodata/memsql.repo 
-sudo yum install -y memsql-client memsql-toolbox memsql-studio
